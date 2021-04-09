74c680c3b5d1e0cd4caec67aebba362640ea3870
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2021-04-09 12:19:39 +0100

    Reactor error response (#11)

--- src/middleware/errorHandlers.js
+++ src/middleware/errorHandlers.js
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import { StatusCodes } from 'http-status-codes';
+import createError from 'http-errors'
 
 config();
 const nodeEnv = process.env.NODE_ENV;
@@ -14,22 +15,8 @@ const { NOT_FOUND, INTERNAL_SERVER_ERROR } = StatusCodes;
  * @returns {Response} not found error
  */
 export const routeNotFound = (req, res, next) => {
-  const error = new Error(`Route -> ${req.method}:${req.originalUrl} not found'`);
+  const error =  createError(404, `Route -> ${req.method}:${req.originalUrl} not found'`);
   res.status(NOT_FOUND);
-  next(error);
-};
-
-
-/**
- * Return a json response with route not found error and 404 status code
- * @param {Request} req 
- * @param {Response} res 
- * @param {NextFunction} next
- * 
- * @returns {Response} not found error
- */
-export const applicationError = (error, res, next) => {
-  res.status(INTERNAL_SERVER_ERROR);
   next(error);
 };
 
@@ -44,9 +31,18 @@ export const applicationError = (error, res, next) => {
  */
 // eslint-disable-next-line no-unused-vars
 export const errorHandler = (error, req, res, _next) => {
-  if (!res.status) res.status(INTERNAL_SERVER_ERROR);
+  let statusCode
+
+  if (error.status) {
+    statusCode = error.status
+  } else if (!res.status) {
+    statusCode = INTERNAL_SERVER_ERROR
+  } else {
+    statusCode = res.status || INTERNAL_SERVER_ERROR
+  }
 
   res
+    .status(statusCode)
     .json({
       success: false,
       message: error.message,
